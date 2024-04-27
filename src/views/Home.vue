e9f7500615be61489ab66e67c17215e4ee85697e
Rovic <joserovic.dimacali@gmail.com>
2024-04-27 13:19:51 +0800

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="pdf-container" style="width: 100%">
+    <VuePdfEmbed
+      source="https://res.cloudinary.com/dpm5vdakr/image/upload/v1714194098/Clementine_Villongco_Portfolio_compressed_l02smq.pdf"
+      style="width: 100%"
+    />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.vue-pdf-embed__page {
+  width: 100% !important;
+}
+</style>
